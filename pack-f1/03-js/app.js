89094fcc345be99e7f688c97a3a4858e8f3a3acf
Phantom <wolfstreetytb046@gmail.com>
2026-09-25 14:09:52 +0200

    trierParPoints pour comparer + explications readme

--- pack-f1/03-js/app.js
+++ pack-f1/03-js/app.js
@@ -10,7 +10,12 @@
 //    DÉCROISSANTS. La liste reçue ne doit pas être modifiée.
 //    À points égaux, celui qui a le plus de victoires passe devant.
 function trierParPoints(liste) {
-  // À COMPLÉTER
+  return liste.slice().sort((a, b) => {
+    if (a.points !== b.points) {
+      return b.points - a.points; // Tri par points décroissants
+    }
+    return b.victoires - a.victoires; // Tri par victoires décroissantes si points égaux
+  });
 }
 
 // 2. remplirTableau(idCorps, liste) : remplit le <tbody> dont l'id est fourni.
